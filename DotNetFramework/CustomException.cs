41c05f919ae3fdb01207001b4482c10bce1f0fb1
nagiyu <otonagiyu@gmail.com>
2024-07-10 20:28:21 +0900

    chore: Add DotNetFramework project files and configuration files

---
+++ DotNetFramework/CustomException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetFramework
+{
+    [Serializable]
+    public class CustomException : Exception
+    {
+        public int ErrorCode { get; private set; }
+
+        public CustomException(string message, int errorCode)
+            : base(message)
+        {
+            ErrorCode = errorCode;
+        }
+
+        protected CustomException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            ErrorCode = info.GetInt32("ErrorCode");
+        }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue("ErrorCode", ErrorCode);
+        }
+    }
+}
---
+++ DotNetFramework/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace DotNetFramework
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                // カスタム例外をスロー
+                throw new CustomException("これはカスタム例外です", 404);
+            }
+            catch (CustomException ex)
+            {
+                Trace.WriteLine($"例外メッセージ: {ex.Message}");
+                Trace.WriteLine($"エラーコード: {ex.ErrorCode}");
+
+                // 例外をシリアル化
+                IFormatter formatter = new BinaryFormatter();
+                using (Stream stream = new FileStream("ExceptionData.bin", FileMode.Create, FileAccess.Write, FileShare.None))
+                {
+                    formatter.Serialize(stream, ex);
+                }
+
+                // シリアル化された例外をデシリアル化
+                using (Stream stream = new FileStream("ExceptionData.bin", FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    CustomException deserializedEx = (CustomException)formatter.Deserialize(stream);
+                    Trace.WriteLine("デシリアル化された例外:");
+                    Trace.WriteLine($"例外メッセージ: {deserializedEx.Message}");
+                    Trace.WriteLine($"エラーコード: {deserializedEx.ErrorCode}");
+                }
+            }
+        }
+    }
+}
